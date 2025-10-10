7017992964321e0090a7bc567b54a18b73e1e38b
rafy <rafy.amgadbenjamin@gmail.com>
2025-10-10 15:26:25 +0200

    fix: linting issues

--- server/app/core/clients/factory_instance.py
+++ server/app/core/clients/factory_instance.py
@@ -1,7 +1,9 @@
 from app.core.clients.client_factory import ClientFactory
-from app.core.source_type import SourceType
 from app.core.clients.read.local_file_system_read import FileSystemDataClient
+from app.core.source_type import SourceType
 
 client_factory = ClientFactory()
-client_factory.register_client(SourceType.FILE, FileSystemDataClient(base_path="./data"))
+client_factory.register_client(
+    SourceType.FILE, FileSystemDataClient(base_path="./data")
+)
 # Register other clients here as needed
